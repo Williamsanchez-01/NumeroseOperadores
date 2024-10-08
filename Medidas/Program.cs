50c60a6aa0f8a615f14efdafabbc10a8c4b3c93f
Williamsanchez-01 <williamcostasanchez@gmail.com>
2024-10-08 04:06:05 -0300

    Finaliza exercicio

---
+++ Medidas/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+
+double metros;
+
+Console.WriteLine("Digite uma medida em metros para saber o equivalente em CM e KM"); 
+metros = Convert.ToDouble (Console.ReadLine());
+
+Console.WriteLine("--- Equivalência --");
+Console.WriteLine($"{metros} m");
+Console.WriteLine($"{metros*1000} cm");
+Console.WriteLine($"{metros/1000:N2} km");
