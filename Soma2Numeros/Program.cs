762b27c87e4843a82761d5add505d58b2079470e
Williamsanchez-01 <williamcostasanchez@gmail.com>
2024-10-08 03:21:29 -0300

    Finaliza Exercicio.

---
+++ Soma2Numeros/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+int n1, n2, soma; 
+
+Console.WriteLine("Digite dois números para saber a soma entre eles.");
+
+Console.Write("primeiro número: ");
+n1 = Convert.ToInt32 (Console.ReadLine());
+
+Console.Write("seguundo número: ");
+n2 = Convert.ToInt32 (Console.ReadLine());
+
+soma = n1+n2;
+
+Console.WriteLine($"A soma desses dois números é : {soma}");
+
