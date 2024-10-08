9e698cd6714ffe47344720fceaf9ac304f045e74
Williamsanchez-01 <williamcostasanchez@gmail.com>
2024-10-08 04:20:24 -0300

    Finaliza exercicio

---
+++ AreaTrianguloRet/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+
+double H,B,A;
+
+Console.WriteLine("Vamos calcular a área de um triângulo retângulo");
+
+Console.WriteLine("Infome a base...: ");
+B = Convert.ToDouble (Console.ReadLine());
+
+
+Console.WriteLine("Informe a altura...:");
+H = Convert.ToDouble (Console.ReadLine());
+
+A = B*H;
+
+Console.WriteLine($"Base..: {B}");
+Console.WriteLine($"Altura..: {H}");
+Console.WriteLine($"Àrea..:{A/2}");
