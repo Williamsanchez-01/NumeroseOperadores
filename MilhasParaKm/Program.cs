86ca4ccc4c84b22813610d4482289f0f145fed19
Williamsanchez-01 <williamcostasanchez@gmail.com>
2024-10-08 03:46:54 -0300

    Finaliza exercicio

--- MilhasParaKm/Program.cs
+++ MilhasParaKm/Program.cs
@@ -1,8 +1,8 @@
 ﻿Console.Clear();
-
+const double KM = 1.609;
 double milhas;
 
 Console.Write("Informe distância em milhas para ser convertida em KM: ");
 milhas = Convert.ToInt32 (Console.ReadLine()); 
 
-Console.WriteLine($"Nas medidas de KM ficará: {milhas*=1.609} ");
+Console.WriteLine($"Nas medidas de KM ficará: {milhas*KM:N3}");
